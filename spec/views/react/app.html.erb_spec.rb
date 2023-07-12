7cb60ceef266b95d8c187e1a6e5efc524774bfea
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-07-11 21:56:29 -0400

    SYS-4 | Setup PWA and refacor HTML

--- spec/views/react/app.html.erb_spec.rb
+++ spec/views/react/app.html.erb_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.describe "react/app.html.erb" do
   it "renders the page" do
     render
-    expect(rendered).to match('<div id="react-app-root" />')
+    expect(rendered).to match('<div id="react-app-root" class="min-h-screen"/>')
   end
 end
