b8056ee725157e331ad9e06e2fdd4141451e1885
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-07-26 23:42:26 -0400

    [sys-20] Configure Redux store (#8)
    * Configure redux store
    * Fix webdrivers

---
+++ react/jest/testUtils.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { RootState, rootReducer } from '../store/store';
+import { BrowserRouter } from 'react-router-dom';
+import { appApi } from '../store/api/appApi';
+
+// As a basic setup, import your same slice reducers
+
+export const s4render = (
+  ui: React.ReactElement,
+  flippers?: Record<string, boolean>,
+  preloadedState?: Partial<RootState>
+) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+      }).concat(appApi.middleware),
+  });
+
+  return render(
+    <Provider store={store}>
+      <BrowserRouter>{ui}</BrowserRouter>
+    </Provider>
+  );
+};
+
+export const s4RenderWithoutRouter = (
+  ui: React.ReactElement,
+  flippers?: Record<string, boolean>,
+  preloadedState?: Partial<RootState>
+) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+      }).concat(appApi.middleware),
+  });
+
+  return render(<Provider store={store}>{ui}</Provider>);
+};
