bd3058f70e5427bc1420fb6f2ba39a98a3202342
Aly Badawy <AlyBadawy@users.noreply.github.com>
2023-07-26 23:08:39 -0400

    Configure redux store

---
+++ react/store/store.ts
@@ -0,0 +1,34 @@
+import {
+  configureStore,
+  PreloadedState,
+  combineReducers,
+} from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { appApi } from './api/appApi';
+import { middlewares } from './middlewares';
+
+export const rootReducer = combineReducers({
+  [appApi.reducerPath]: appApi.reducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+      }).concat(middlewares),
+  });
+
+const store = setupStore();
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
